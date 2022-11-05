0ea7fd49d6893a04e153872d6c2520aca9d4dc1d
jackcheng1021 <jackcheng1021@126.com>
2022-11-05 18:41:44 +0800

    upddate install cloud and saltstack

--- devops-pre-install.sh
+++ devops-pre-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "setup devops-salt"
+echo "setup devops-pre"
 
 source devops-openrc.sh
 
@@ -23,16 +23,116 @@ if [ $? -ne 0 ]; then
   exit
 fi
 
-echo "prepare the environment for saltstack"
-n=$(cat /etc/yum.repos.d/CentOS-Base.repo | grep -o "aliyun\.com" | wc -l)
-if [ $n -le 15 ]; then
+echo "config base yum"
+n=$(cat /etc/yum.repos.d/CentOS-Base.repo | grep -o "mirrors\.aliyun\.com" | wc -l)
+if [ $n -lt 15 ]; then
   curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo &> /dev/null
+  sed -i -e '/mirrors.cloud.aliyuncs.com/d' -e '/mirrors.aliyuncs.com/d' /etc/yum.repos.d/CentOS-Base.repo
 fi
+yum makecache &> /dev/null
+
+echo "prepare cloud env"
+
+echo "install git"
+yum -y install git &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "install git error"
+  exit
+fi
+
+echo "download cloud script"
+{
+  git clone https://github.com/jackcheng1021/liberty_openstack.git &> /dev/null
+}&
+wait
+
+if [ $? -ne 0 ]; then
+  echo "please unset git proxy and reboot the script"
+  exit
+fi
+
+echo "please input cloud openrc use vi"
+sleep 2
+vi liberty_openstack/liberty-openrc.sh
+
+echo "prepare cloud env finish"
+
+echo "setup cloud"
+cd liberty_openstack/
+ln -s liberty-env-config.sh /usr/local/bin/liberty-env-config
+liberty-env-config
+liberty-pre-controller
+
+spawn ssh -p 22 $compute01_user@$compute01_ip
+expect \"Password:\"
+send \"${compute01_user_pass}\r\"
+expect \"${compute01_user}@*\"
+send \"liberty-pre-compute\r\"
+expect \"${compute01_user}@*\"
+send \"exit\r\"
+expect eof
+
+spawn ssh -p 22 $compute02_user@$compute02_ip
+expect \"Password:\"
+send \"${compute02_user_pass}\r\"
+expect \"${compute02_user}@*\"
+send \"liberty-pre-compute\r\"
+expect \"${compute02_user}@*\"
+send \"exit\r\"
+expect eof
+
+liberty-database-controller
+liberty-keystone-controller
+liberty-glance-controller
+liberty-nova-controller
+    
+spawn ssh -p 22 $compute01_user@$compute01_ip
+expect \"Password:\" 
+send \"${compute01_user_pass}\r\"
+expect \"${compute01_user}@*\"
+send \"liberty-nova-compute ${compute01_ip}\r\"
+expect \"${compute01_user}@*\"
+send \"exit\r\"
+expect eof
+
+spawn ssh -p 22 $compute02_user@$compute02_ip
+expect \"Password:\" 
+send \"${compute02_user_pass}\r\"
+expect \"${compute02_user}@*\"
+send \"liberty-nova-compute ${compute02_ip}\r\"
+expect \"${compute02_user}@*\"
+send \"exit\r\"
+expect eof
+
+liberty-neutron-controller
+
+spawn ssh -p 22 $compute01_user@$compute01_ip
+expect \"Password:\"
+send \"${compute01_user_pass}\r\"
+expect \"${compute01_user}@*\"
+send \"liberty-neutron-compute ${compute01_ip}\r\"
+expect \"${compute01_user}@*\"
+send \"exit\r\"
+expect eof
+
+spawn ssh -p 22 $compute02_user@$compute02_ip
+expect \"Password:\"
+send \"${compute02_user_pass}\r\"
+expect \"${compute02_user}@*\"
+send \"liberty-neutron-compute ${compute02_ip}\r\"
+expect \"${compute02_user}@*\"
+send \"exit\r\"
+expect eof
+
+echo "setup cloud finish"
+
+echo "setup saltstack"
+echo "prepare the environment for saltstack"
 rpm --import https://repo.saltproject.io/py3/redhat/7/x86_64/latest/SALTSTACK-GPG-KEY.pub &> /dev/null
 curl -fsSL https://repo.saltproject.io/py3/redhat/7/x86_64/latest.repo | tee /etc/yum.repos.d/salt.repo &>/dev/null
 yum clean expire-cache &> /dev/null
 
-echo "install saltstack"
+echo "install saltstack for controller and compute node"
 yum -y install salt-master &> /dev/null
 if [ $? -ne 0 ]; then
   echo "controller: salt-master install failed"
@@ -153,4 +253,4 @@ if [ $? -ne 0 ]; then
   exit
 fi
 
-echo "devops-salt setup finish"
+echo "setup saltstack finish"
